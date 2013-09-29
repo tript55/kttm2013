7e62b48751d49c64bdd508f798625f26b13cb853
tript55 <trongtri92nd@gmail.com>
2013-09-29 21:40:13 +0700

    djfojfsof

---
+++ triangle/test.py
@@ -0,0 +1,74 @@
+'''
+Created on Sep 28, 2013
+
+@author: trer
+'''
+import unittest
+import decimal
+from triangle import detect_triangle
+
+e = decimal.Decimal('10')**-9
+class TestUM(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    def test0(self):
+        self.assertEqual( detect_triangle(3,4,6), "thuong")
+    def test1(self):
+        self.assertEqual( detect_triangle(2,2,2), "deu")
+    def test2(self):
+        self.assertEqual( detect_triangle(4,4,6), "can")
+    def test4(self):
+        self.assertEqual( detect_triangle(4,6,4), "can")
+    def test5(self):
+        self.assertEqual( detect_triangle(6,4,4), "can")
+    def test6(self):
+        self.assertEqual( detect_triangle(3,4,5), "vuong")
+    def test7(self):
+        self.assertEqual( detect_triangle(4,5,3), "vuong")
+    def test8(self):
+        self.assertEqual( detect_triangle(5,4,3), "vuong")
+    def test9(self):
+        self.assertEqual( detect_triangle(0,3,5), "wronginput")
+    def test10(self):
+        self.assertEqual( detect_triangle(3,0,5), "wronginput")
+    def test11(self):
+        self.assertEqual( detect_triangle(3,5,0), "wronginput")
+    def test12(self):
+        self.assertEqual( detect_triangle(-1,3,5), "wronginput")
+    def test13(self):
+        self.assertEqual( detect_triangle(3,-1,5), "wronginput")
+    def test14(self):
+        self.assertEqual( detect_triangle(3,5,-1), "wronginput")
+    def test15(self):
+        self.assertEqual( detect_triangle(3,4,8), "khongphaitamgiac")
+    def test16(self):
+        self.assertEqual( detect_triangle(3,8,4), "khongphaitamgiac")
+    def test17(self):
+        self.assertEqual( detect_triangle(8,3,4), "khongphaitamgiac")
+    def test18(self):
+        self.assertEqual( detect_triangle(4,e,4), "can")
+    def test19(self):
+        self.assertEqual( detect_triangle(e,4,4), "can")
+    def test20(self):
+        self.assertEqual( detect_triangle(4,4,e), "can")
+    def test21(self):
+        self.assertEqual( detect_triangle(2**32-1,2**32-3,4),"thuong")
+    def test22(self):
+        self.assertEqual( detect_triangle(2**32-3,2**32-1,4), "thuong")
+    def test23(self):
+        self.assertEqual( detect_triangle(2**32-3,4,2**32-1), "thuong")
+    def test24(self):
+        self.assertEqual( detect_triangle(2**32-1,2**32-1,2**32-1), "deu")
+    def test25(self):
+        self.assertEqual( detect_triangle(2**32,5,4), "wronginput")
+    def test26(self):
+        self.assertEqual( detect_triangle(4,2**32,5), "wronginput")
+    def test27(self):
+        self.assertEqual( detect_triangle(4,5,2**32), "wronginput")
+    def test28(self):
+        self.assertEqual( detect_triangle(e,e,e),"deu")
+    def test29(self):
+        self.assertEqual( detect_triangle(e,2**32-1,2**32-1),"can")
+if __name__ == '__main__':
+    unittest.main()
---
+++ triangle/triangle.py
@@ -0,0 +1,54 @@
+'''
+Created on Sep 26, 2013
+
+@author: trer
+'''
+import decimal
+import math
+e = decimal.Decimal('10')**-9
+maxf = 2**32
+def check_can(a,b,c):
+    if (a == b or b == c or a ==c):
+        return 1
+    return 0
+def check_deu(a,b,c):
+    if ( a== b and b ==c ):
+        return 1
+    return 0
+def check_input(a,b,c):
+    if ( a < maxf and b < maxf and c < maxf and a >= e and b >= e and c >= e ):
+        return 1
+    return 0
+def check_vuong(a,b,c):
+    if b > c:
+        d = b
+        b = c
+        c = d
+    if a > c:
+        d = a
+        a = c
+        c = d
+    if ( a**2+b**2-c**2) < e**2:
+        return 1
+    return 0
+def check_triangle(a,b,c):
+    if (( a+b ) > c and ( b+c ) > a and ( a+c ) > b):
+        return 1
+    return 0
+def detect_triangle(a,b,c):
+    if check_input(a,b,c):
+        if check_triangle(a,b,c):
+            if check_deu(a,b,c):
+                return "deu"
+            elif check_vuong(a,b,c) and check_can(a,b,c):
+                return "vuongcan"
+            elif check_vuong(a,b,c):
+                return "vuong"
+            elif check_can(a,b,c):
+                return "can"
+            else:
+                return "thuong"
+        else:
+            return "khongphaitamgiac"
+    else:
+        return "wronginput"  
