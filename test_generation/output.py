edf7a299dedf18ef4957422ff4c5068e10892947
tript55 <trongtri92nd@gmail.com>
2013-10-20 10:25:39 +0700

    ajdflsjdflsdjfs

--- test_generation/output.py
+++ test_generation/output.py
@@ -8,26 +8,36 @@ import re
 import unittest
 import itertools
 from random import randint
-f = open("input.py")
-cmt = ["'''"]
-lineslist = f.readlines()
-classes = []
-lval = []
-for i in range(0,len(lineslist)):
-    line = lineslist[i]
+def importfile(filename):
+    f = open(filename)
+    cmt = ["'''"]
+    lineslist = f.readlines()
+    classes = []
+    lval = []
+    for i in range(0,len(lineslist)):
+        line = lineslist[i]
     
-    if line.startswith("def main") and "'''" in lineslist[i+1]:
-        for j in range(i+2,len(lineslist)):
-            if "'''" in lineslist[j]:
-                break
-            ne = re.findall(r'\d+',lineslist[j])
-            k = 0;
-            selectednums = []
-            while (k < len(ne)):
-                x = randint(int(ne[k]),int(ne[k+1]))
-                selectednums.append(x)
-                k = k+2
-            lval.append(selectednums)
+        if line.startswith("def main") and "'''" in lineslist[i+1]:
+            for j in range(i+2,len(lineslist)):
+                if "'''" in lineslist[j]:
+                    break
+                ne = re.findall(r'\d+',lineslist[j])
+                k = 0;
+                selectednums = []
+                
+                while (k < len(ne)):
+                    if int(ne[k]) > int(ne[k+1]):
+                        raise Exception("wrong input")
+                    l = k +2
+                    while (l < len(ne)):
+                        if int(ne[k]) < int(ne[l+1]) and int(ne[l]) < int(ne[k+1]):
+                            raise Exception("wrong input")
+                        l = l+2
+                    x = randint(int(ne[k]),int(ne[k+1]))
+                    selectednums.append(x)
+                    k = k+2
+                lval.append(selectednums)
+    return lval
 class TestSequense(unittest.TestCase):
     pass
 def test_generator(a,b):
@@ -37,6 +47,7 @@ def test_generator(a,b):
 if __name__ == '__main__':
     m = 0;
     b ="socio"
+    lval = importfile("input.py")
     for i in itertools.product(*lval):
         test_name = 'test_%d' %m
         m = m+1
